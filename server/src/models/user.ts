922f0011a3f904e3aaec6b9757fe4ec315b2c1c6
lattepunk-coding <174910592+lattepunk-coding@users.noreply.github.com>
2025-01-19 11:05:16 -0500

    converted a few files from postgres to mongo. needs more files and testing

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,59 +1,49 @@
-import { DataTypes, Sequelize, Model, Optional } from 'sequelize';
-import bcrypt from 'bcrypt';
+import { Schema, model, Document } from "mongoose";
+import bcrypt from "bcrypt";
 
-interface UserAttributes {
-  id: number;
+export interface IUser extends Document {
   username: string;
   password: string;
 }
 
-interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
-
-export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-  public id!: number;
-  public username!: string;
-  public password!: string;
-
-  public readonly createdAt!: Date;
-  public readonly updatedAt!: Date;
-
-  // Hash the password before saving the user
-  public async setPassword(password: string) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(password, saltRounds);
-  }
-}
-
-export function UserFactory(sequelize: Sequelize): typeof User {
-  User.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
     },
-    {
-      tableName: 'users',
-      sequelize,
-      hooks: {
-        beforeCreate: async (user: User) => {
-          await user.setPassword(user.password);
-        },
-        beforeUpdate: async (user: User) => {
-          await user.setPassword(user.password);
-        },
-      }
-    }
-  );
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    // Mongoose will automatically manage createdAt and updatedAt fields:
+    timestamps: true,
+  },
+);
 
-  return User;
-}
+// Hash password before saving (for create and update):
+userSchema.pre<IUser>("save", async function (next) {
+  // Only re-hash if the password key has been modified (or is new)
+  if (this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+// Hash password on findOneAndUpdate:
+userSchema.pre("findOneAndUpdate", async function (next) {
+  // "this" is the Query object, so we get the update from "this.getUpdate()"
+  const update = this.getUpdate() as { password?: string };
+  if (update.password) {
+    const saltRounds = 10;
+    update.password = await bcrypt.hash(update.password, saltRounds);
+  }
+  next();
+});
+
+const User = model<IUser>("User", userSchema);
+
+export default User;
