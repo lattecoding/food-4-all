05898d0dcf9b81e3470901463591cf7784e024dd
naiknetra <naiknetra5@gmail.com>
2025-01-30 18:36:16 -0500

    added Favorites file

--- client/src/pages/Favorites.tsx
+++ client/src/pages/Favorites.tsx
@@ -1,6 +1,22 @@
 //This will be Netra’s page to add in favorites jpeg
 // Need to write the code the will allow  user to click on Favorites icon (this is how it’s labeled in Board.tsx to reference) <BottomNavigationAction label=“Favorites” icon={<FavoriteIcon />} />, the user should be redirected to favorites page in which our ‘dummy’ favorites will display.
 
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+
+/*const Board = () => {// Inside Board component
+return (
+    <BottomNavigation>
+        <BottomNavigationAction
+            label="Favorites"
+            icon={<FavoriteIcon />}
+            component={Link}
+            to="/favorites"
+        />
+        {/* Other navigation actions */}
+    
+
 const Favorites = () => {
     const dummyFavorites = [
         { id: 1, name: 'Favorite Item 1' },
